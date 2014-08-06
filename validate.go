bcd67ec6aa1efe20fd0a035fe3ee16c49086fa4b
tikiatua <tikiatua@gmail.com>
2014-08-06 10:26:55 +0200

    refine our html report further

--- validate.go
+++ validate.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 
 	"path/filepath"
 
 	"archive/zip"
 	"bytes"
-	"io"
-	"os"
 
 	"regexp"
 	"strings"
@@ -17,66 +17,98 @@ import (
 	"time"
 
 	"html/template"
-
+	"github.com/skratchdot/open-golang/open"
+	"log"
 )
 
 // define some custom regular expressions to find hyperlinks in our word documents
-var (
-	hyperlinkExpression = regexp.MustCompile(`Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/hyperlink" Target="(?P<url>.+?)"`)
-	//hyperlinkExpression = regexp.MustCompile(`Target="(?P<url>.+?)"`)
-)
+// we define this globally to avoid recompilation
+var hyperlinkExpression = regexp.MustCompile(`Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/hyperlink" Target="(?P<url>.+?)"`)
+
+// we define the name of our report
+var reportName string = "report"
 
 // define a custom document structure
 type Document struct {
-	Path         string
-	Hyperlinks   []string
+	Path       string
+	IsValid    bool
+	Hyperlinks []Hyperlink
 }
 
+// define a custom hyperlink structure
 type Hyperlink struct {
-	url string
-	working bool
+	Url     string
+	IsWorking bool
 }
 
-// create a report structure for our output
+// define a custom report structre
 type Report struct {
-	Documents []Document
-	InvalidLinks []string
+	ResultOfValidation bool
+	Directories        []string
+	Documents          *[]Document
+	InvalidHyperlinks  []Hyperlink
+	Date string
 }
 
 // our main function
 func main() {
 
-	// get a list of all files in the directories specified
-	wordDocuments := getAllWordDocumentPathsInDirectory(".")
+	// we are only interested in the current directory
+	directories := []string{"."}
 
-	invalidLinks := []string{}
+	// get current date and time
+	currentTime := time.Now().String()
+	currentTime = currentTime[:19]
+
+	// get a list of all files in the directories specified
+	wordDocuments := getDocxFilesInDirectory(".")
+
+	// initialize our report structure
+	report := Report{
+		Directories: directories,
+		Documents: &wordDocuments,
+		Date: currentTime,
+	}
 
 	// go through all word documents and extract their hyperlinks
-	for index, _ := range wordDocuments {
+	for index := range wordDocuments {
 
-		fmt.Println(wordDocuments[index].Path)
+		// set word document validity initially to valid
+		wordDocuments[index].IsValid = true;
 
-		wordDocuments[index].findHyperlinks()
+		// get hyperlinks from the word file
+		hyperlinks := extractHyperlinksFromDocxFile(wordDocuments[index].Path)
 
-		for _, link := range wordDocuments[index].Hyperlinks {
+		// iterate through all links
+		for _, link := range hyperlinks {
 
-			if isHyperlinkWorking(link) == false {
+			link.IsWorking = isHyperlinkWorking(link)
 
-				invalidLinks = append(invalidLinks, link)
-				fmt.Println("- " + link)
+			wordDocuments[index].Hyperlinks = append(wordDocuments[index].Hyperlinks, link)
+
+			if link.IsWorking == false {
+
+				// word document is not valid
+				wordDocuments[index].IsValid = false
+
+				// remember our invalid hyperlinks
+				report.InvalidHyperlinks = append(report.InvalidHyperlinks, link)
+
 			}
 
 		}
 	}
 
-	// fmt.Println(wordDocuments)
+	// create an html report with our data
+	report.create()
 
-	createReport(&wordDocuments)
+	// open the report
+	report.open()
 
 }
 
 // get paths for all word documents in the specified directories
-func getAllWordDocumentPathsInDirectory(rootDirectory string) []Document {
+func getDocxFilesInDirectory(rootDirectory string) []Document {
 
 	// initialize a new slice of documents
 	wordFiles := []Document{}
@@ -105,23 +137,20 @@ func getAllWordDocumentPathsInDirectory(rootDirectory string) []Document {
 }
 
 // add a method to our document to extract all hyperlinks
-func (doc *Document) findHyperlinks() bool {
+func extractHyperlinksFromDocxFile(filePath string) []Hyperlink {
 
 	// get file content
-	content := getFileContentAsString(doc.Path)
+	content := getLinkFileContent(filePath)
 
-	// find all hyperlinks in the document (this will unfortunately also return some microsoft schema links)
-	matches := extractHyperlinks(content)
+	// find all hyperlinks in the document
+	matches := extractHyperlinksFromContent(content)
 
 	// store the hyperlinks in a the document reference
-	doc.Hyperlinks = matches
-
-	// extraction of hyperlinks successfully completed
-	return true
+	return matches
 
 }
 
-func getFileContentAsString(filePath string) string {
+func getLinkFileContent(filePath string) string {
 
 	// open the docx file with our zip module (as it is basically a container)
 	docxContainer, err := zip.OpenReader(filePath)
@@ -162,18 +191,18 @@ func getFileContentAsString(filePath string) string {
 
 }
 
-func extractHyperlinks(fileContent string) []string {
+func extractHyperlinksFromContent(fileContent string) []Hyperlink {
 
 	// find all matching links (the url of the hyperlink is matched by a capture group)
 	matches := hyperlinkExpression.FindAllStringSubmatch(fileContent, -1)
 
 	// initialize a new slice of strings of the same length as our matches
-	var links []string = make([]string, len(matches))
+	var links []Hyperlink = make([]Hyperlink, len(matches))
 
 	// we are only interested in the second element in our list as
 	// the Submatch function returns the full match as first element and our capture group as second
 	for index, match := range matches {
-		links[index] = match[1]
+		links[index] = Hyperlink{Url: match[1], IsWorking: false}
 	}
 
 	// no filter out all microsoft links
@@ -183,20 +212,20 @@ func extractHyperlinks(fileContent string) []string {
 
 }
 
-func filterHyperlinks(hyperlinks []string) []string {
+func filterHyperlinks(hyperlinks []Hyperlink) []Hyperlink {
 
 	// regular expression to find microsoft links
 	var microsoft string = `http://office.microsoft.com`
 
 	// initialize an empty slice of strings
-	var filteredLinks = []string{}
+	var filteredLinks = []Hyperlink{}
 
 	// check all links
 	for _, link := range hyperlinks {
 
 		// exclude any microsoft links
-		isMicrosoft := strings.Contains(link, microsoft)
-		isEmpty := (link == "")
+		isMicrosoft := strings.Contains(link.Url, microsoft)
+		isEmpty := (link.Url == "")
 
 		// include only non microsoft links
 		if isMicrosoft == false && isEmpty == false {
@@ -209,12 +238,12 @@ func filterHyperlinks(hyperlinks []string) []string {
 
 }
 
-func isHyperlinkWorking(link string) bool {
+func isHyperlinkWorking(link Hyperlink) bool {
 
 	// issue a GET request to the specified url and wait for response
 	// set a timeout if there is no response
 	_, err := goreq.Request{
-		Uri:     link,
+		Uri:     link.Url,
 		Timeout: 1500 * time.Millisecond,
 	}.Do()
 
@@ -228,24 +257,68 @@ func isHyperlinkWorking(link string) bool {
 
 }
 
+func getAbsoluteFilePath (path string) string {
 
-func createReport(documents *[]Document) {
+	// check if the path is already absolute
+	if filepath.IsAbs(path) {
+		// if so, return the path
+		return path
+	}
 
-	file, _ := os.Create("report.html")
+	// try to find the absolute path
+	absolutePath, err := filepath.Abs(path)
+
+	if err != nil {
+		// we return the original path, if there was an error
+		return path
+	} else {
+		return absolutePath
+	}
+
+}
+
+// create a custom report
+func (report *Report) create() bool {
+
+	// open a new file to write our report to
+	file, _ := os.Create(reportName + ".html")
 	defer file.Close()
 
-	tmpl, err := template.ParseFiles("report.tpl")
+	functionMap := template.FuncMap {
+		"absolutePath": getAbsoluteFilePath,
+	}
+
+	// load our template from the templat file
+	tmpl, err := template.New("report").Funcs(functionMap).ParseFiles(reportName + ".tmpl")
 
 	if err != nil {
 		panic(err)
+		return false
 
 	} else {
 
-		err = tmpl.ExecuteTemplate(file, "report.tpl", documents)
+		// fill our template with content and write it to the file
+		err = tmpl.ExecuteTemplate(file, reportName+".tmpl", report)
 
 		if err != nil {
-			fmt.Println("error while executing the template")
+			panic(err)
+			fmt.Println("Could not fill the template with report data")
+			return false
 		}
 	}
+
+	return true
+
+}
+
+func (report *Report) open() {
+
+	// open the report in the default browser
+	err := open.Start(reportName + ".html")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 
 }
