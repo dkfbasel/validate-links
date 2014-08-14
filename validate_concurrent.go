f506fa2e41523e9db2a82410b391e3017a6dbbc7
tikiatua <tikiatua@gmail.com>
2014-08-14 11:51:45 +0200

    adapt process to work concurrently

--- validate_concurrent.go
+++ validate_concurrent.go
@@ -24,17 +24,6 @@ import (
 
 func main() {
 
-	// fmt.Println("get documents")
-
-	// var documents []*Document = getAndCheckFilesInDirectory(".")
-
-	// for _, value := range documents {
-	// 	fmt.Println(value.Path + " - " + value.Type)
-	// }
-
-	// fmt.Println("create report")
-	// fmt.Println("be done")
-
 	// measure execution time
 	start := time.Now()
 
@@ -53,10 +42,14 @@ func main() {
 	// get a list of all files in the directories specified
 	documents := getAndCheckFilesInDirectory(".")
 
+	// TODO: assess whether a file contains invalid links
+
+	// TODO: assess  whether there are any invalid files
+
 	// initialize our report structure
 	report := Report{
-		Directories: make([]string{"."}),
-		Documents:   &documents,
+		Directories: directories,
+		Documents:   documents,
 		Date:        currentTime,
 	}
 
@@ -88,17 +81,26 @@ type Hyperlink struct {
 	IsWorking bool
 }
 
-// define a custom report structre
-type Report struct {
-	ResultOfValidation bool
-	Directories        []string
-	Documents          *[]Document
-	InvalidHyperlinks  []Hyperlink
-	Date               string
-}
+func (link *Hyperlink) validate(wg *sync.WaitGroup) {
 
-// we define the name of our report
-var reportName string = "report"
+	// issue a GET request to the specified url and wait for response
+	// set a timeout of 10 seconds if there is no response
+	_, err := goreq.Request{
+		Uri:     link.Url,
+		Timeout: 10000 * time.Millisecond,
+	}.Do()
+
+	if err != nil {
+		// link was not found
+		link.IsWorking = false
+		wg.Done()
+	} else {
+		// link was found
+		link.IsWorking = true
+		wg.Done()
+	}
+
+}
 
 // define some custom regular expressions
 var matchers map[string]*regexp.Regexp
@@ -117,39 +119,42 @@ func initializeMatchers() {
 	matchers["mailto"] = regexp.MustCompile(`mailto:.*`)
 }
 
-func getAndCheckFilesInDirectory(rootDirectory string) []*Document {
+func getAndCheckFilesInDirectory(rootDirectory string) []Document {
 
 	// initialize a new slice of documents
-	documents := []*Document{}
+	documents := []Document{}
 
 	var fileChannel chan Document = make(chan Document)
-
-	var doneChannel chan bool = make(chan bool)
 
 	var wg sync.WaitGroup
 
 	// we have to wait until all file walking is done
+	wg.Add(1)
 
 	// walk recursively through our directory in a separate thread
+	// send each matching file into our file channel
 	go walkDirectory(rootDirectory, fileChannel, &wg)
 
+	// for each file we find, we get all links and validate them
 	for file := range fileChannel {
 
-		// add a reference to the file to our documents slice
-		documents = append(documents, &file)
+		// fmt.Println("- checking file: " + file.Path)
 
 		// remember to wait until the document is fully checked
-		wg.Add(1)
+		var checkingHyperlinks sync.WaitGroup
+
+		checkingHyperlinks.Add(1)
 
 		// check hyperlinks of the document
-		go extractAndCheckHyperlinks(&file, &wg)
+		go extractAndCheckHyperlinks(&file, &checkingHyperlinks)
+
+		checkingHyperlinks.Wait()
+
+		documents = append(documents, file)
 
 	}
 
-	// file walking is done
-	<-doneChannel
-
-	// document checking is done
+	// we are finished with finding and checking all elements
 	wg.Wait()
 
 	fmt.Println("we are done with these files")
@@ -190,15 +195,29 @@ func walkDirectory(directory string, fileChannel chan Document, wg *sync.WaitGro
 
 }
 
-func extractAndCheckHyperlinks(doc *Document, wg *sync.WaitGroup) {
-	fmt.Println("we are changing the type")
-	doc.Type = "this is a new type"
+func extractAndCheckHyperlinks(file *Document, wg *sync.WaitGroup) {
+
+	// get all hyperlinks from the document
+	file.Hyperlinks = extractHyperlinksFromDocument(*file)
+
+	// check all hyperlinks in a separate routine
+	for index, _ := range file.Hyperlinks {
+
+		wg.Add(1)
+
+		fmt.Println("-- checking link: " + file.Hyperlinks[index].Url)
+
+		go file.Hyperlinks[index].validate(wg)
+
+	}
+
 	wg.Done()
+
 }
 
-func extractHyperlinkFromDocument(document Document) []Hyperlink {
+func extractHyperlinksFromDocument(document Document) []Hyperlink {
 
-	// get file content
+	// get the content of the file containing the links
 	content := getLinkFileContent(document)
 
 	// find all hyperlinks in the document
@@ -295,25 +314,6 @@ func filterHyperlinks(hyperlinks []Hyperlink) []Hyperlink {
 
 }
 
-func isHyperlinkWorking(link Hyperlink) bool {
-
-	// issue a GET request to the specified url and wait for response
-	// set a timeout of 10 seconds if there is no response
-	_, err := goreq.Request{
-		Uri:     link.Url,
-		Timeout: 10000 * time.Millisecond,
-	}.Do()
-
-	if err != nil {
-		// link was not found
-		return false
-	} else {
-		// link was found
-		return true
-	}
-
-}
-
 func getAbsoluteFilePath(path string) string {
 
 	// check if the path is already absolute
@@ -334,7 +334,16 @@ func getAbsoluteFilePath(path string) string {
 
 }
 
-// create a custom report
+// define a custom report structre
+type Report struct {
+	ResultOfValidation bool
+	Directories        []string
+	Documents          []Document
+	InvalidHyperlinks  []Hyperlink
+	Date               string
+}
+
+// create a custom html report
 func (report *Report) create() bool {
 
 	// open a new file to write our report to
@@ -382,3 +391,206 @@ func (report *Report) open() {
 // we define the name of our report
 var reportName string = "report"
 
+const reportTemplate = `<html>
+<head>
+<title>Hyperlinks in Word Dateien überprüfen</title>
+<meta charset="utf-8">
+<meta name="author" content="Dr. med. Ramon Saccilotto, DKF, Universitätsspital Basel">
+
+<style type="text/css">
+
+* {
+font-family: "Helvetica Neue", "Helvetica", "Calibri", "Arial", sans-serif;
+font-weight: normal;
+font-size: 14px;
+}
+
+a {
+text-decoration: none;
+color: inherit;
+font-size: inherit;
+}
+
+body {
+background-color: #eaeaea;
+}
+
+.container {
+min-width: 600px;
+margin: 40px 20px 20px 20px;
+padding: 20px;
+padding-bottom: 40px;
+border: 1px solid #ccc;
+background-color: #fff;
+box-shadow: 0px 1px 1px rgba(74, 69, 69, 0.6), 0px -1px 1px rgba(50, 50, 50, 0.05);
+}
+
+.info {
+margin: 20px;
+}
+
+.info p {
+font-size: 12px;
+opacity: 0.2;
+}
+
+.info:hover p{
+opacity: 1;
+transition: opacity 500ms;
+}
+
+h1 {
+margin: 0px;
+padding: 0px;
+margin-bottom: 15px;
+font-size: 20px;
+border-bottom: 1px solid #ccc;
+font-weight: bold;
+padding-bottom: 10px;
+
+}
+
+ul {
+margin: 0px;
+padding: 0px;
+list-style-type: none;
+padding-left: 5px;
+}
+
+ul li {
+position: relative;
+padding-left: 28px;
+}
+
+ul.directories > li + li{
+margin-top: 20px;
+}
+
+ul li:before {
+content: "";
+background-position: top left;
+background-repeat: no-repeat;
+display: block;
+position: absolute;
+left: 0px;
+top: 0px;
+width: 20px;
+height: 20px;
+}
+
+ul.directories {
+margin-bottom: 40px;
+}
+
+ul.directories li:before {
+background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAj0lEQVQ4T2NkYGBgBGJ2IIaBv0DGbyQ+XiZI81Ig/gLE/6EqFYB0PRCfJMYQkAELgDgBSTEHkD2DCM0gvaewGUCEXriSdSADzgPxLSQvEGsASK8aiLgPxIrE6kJTdx9kwB0gViHTgDtUMeAo0HZrMl1wlNJYWDBqAAM4DNAzE7ERAtLLAyLQszOxBoDU/QQAylQgG9KLVSEAAAAASUVORK5CYII=);
+}
+
+ul.documents > li:before {
+background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAABqElEQVQ4T52VPSiFURjH3VDyMRhEDK6Uj0wG+VgskhSDDJLMmCUD8p2S7RbJxKZMLGKgJBsGJKQsZCeUj9//dl699ziXk1O/3vs873P+5/8+57zvjaQkjhHCbCvnCs9JrrpuRKzkNXGfh+AKNUswZ9faghcUVHoInlHzAhswG64PBNNM8pBrgyX4Sfxu5STYCmuwA1PBfQnWmZVukzirIp/rEGwk9wRbcAyDqgkEe/i9D5dwB1lGoJqrNkqLhoccqt+vkAMtMA4TgWA3QTnMQA08wxWUQa9DUK4zrEXWiUsDwS4C9enIONrl+gGjsGcE1dtmS0ThpKmNb2hYUI/bDidQDxKdNovokaNQ4RDcJqeN+yGo5sagFh6NwH1IUJvQ5hAcSuZQ1uVAR0cTN81ktUEOi6DEIXhgcgkOl42Qoz7ehkJogk5HwYDtMJOEdlNjEfqtSerPKeQZl7ameq7x7TBcELx6+SQXXHYdOR0rnYhfBdMpKPYU1AH/02EBFfqa+IwOH4eppmc+gg8+Dn2E7BpnD2+oGv6PGnPmIWp/YH3/AlxrvpEc+wLSwV8VusxZtAAAAABJRU5ErkJggg==);
+top: -2px;
+}
+
+ul.documents > li + li {
+margin-top: 25px;
+}
+
+div.result {
+border-width: 1px;
+border-style: solid;
+border-radius: 2px;
+padding: 10px;
+margin-bottom: 25px;
+}
+
+div.result.valid {
+border-color: #20d420;
+background-color: #dcffe7;
+}
+
+div.result.invalid {
+border-color: #db2d2d;
+background-color: #fff5f5;
+}
+
+h2 {
+font-weight: bold;
+}
+
+ul.links li{
+font-size: 12px;
+}
+
+ul.links > li + li {
+margin-top: 15px;
+}
+
+ul.links li.invalid:before {
+background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAA/UlEQVQ4T2NkoBAwUqifAa8B/xkYjIAW/AcqOo/LIpwGADVLAjVdBxkAxFpAhc+xGYLPgHVADYFQTeuACoOJNgBopT9Q8QY0Df5AQzahG4LhAqBmLqCiy0CsBMTToBqygPQ9INYBaviObAg2A3qACoqhihqhdD2U7gFqKMVpANB2Q6DkKSBmwWHAH6C4GXKswF0A1AxiHwJiGyQbJkPZuUhiR4BsO6BiUOwg0gGQB/LnVGwhjUUsC2jAdLgBQM0iQM5NIBZCU7wCakk4mvg7IF8daMgbsBeABiQAqflE2g5TFg/UvAhmAD9QFGQIB5GG/ABZCNT8ibaZiRjXAABQjy8Rw0RFZAAAAABJRU5ErkJggg==);
+top: -1px;
+}
+
+ul.links li.valid:before {
+background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAA6UlEQVQ4T2NkoDJgpLJ5DKMGEhei/Ve1DRj/M/T///01sNDwwQdkXSSHIdSw/YyMDAL//zMcKNC56ki2gWiGffjL8NuhWOfWRbIMJMYwkMEYXoZo/J9foHMtEWZz/2UNB0ZG5vVQb2J1GUwthoETrmidZ2RkNPj3769joe6NA0ALEpgYGOaDNADDDK9hWF3Ye0VNn4WR9cJ/hv8P/v//P5GJkamfWMOwGggSnHhZM5+BiWkCzBvEuAynl2ESQK/PB3o9gRTDcLoQJNFxRomfg4NzAzBpFKAnDXzJn+SETSgvjRpIKIQIywMAWmd1FTm7YC8AAAAASUVORK5CYII=);
+top: -3px;
+}
+
+.valid {
+color: #20d420;
+}
+
+.invalid {
+color: #db2d2d;
+}
+
+
+
+</style>
+</head>
+<body>
+<div class="container">
+
+<h1>Untersuchtes Verzeichnis</h1>
+
+<ul class="directories">
+{{range .Directories}}
+<li><a href="file:///{{absolutePath .}}">{{absolutePath .}}</a></li>
+{{end}}
+</ul>
+
+<h1>Resultat der Link-Validierung</h1>
+
+
+{{if .ResultOfValidation}}
+<div class="result valid">
+Alle Dateien enthalten nur gültige Links
+</div>
+{{else}}
+<div class="result invalid">
+Leider gibt es Dateien mit ungültigen Links
+</div>
+{{end}}
+
+<ul class="documents">
+{{range .Documents}}
+<li class="result">
+<h2 class="{{if .IsValid}}valid{{else}}invalid{{end}}"><a href="file:///{{absolutePath .Path}}">{{.Path}}</a></h2>
+
+<ul class="links">
+{{range .Hyperlinks}}
+<li class="result {{if .IsWorking}}valid{{else}}invalid{{end}}"><a href="{{.Url}}">{{.Url}}</a></li>
+{{end}}
+</ul>
+</li>
+{{end}}
+</ul>
+
+</div>
+
+<div class="info">
+<p class="time">Ausgeführt am: {{.Date}}</p>
+<p class="author">&copy;&nbsp;2014, Department Klinische Forschung, Universitätsspital Basel</p>
+</div>
+</body>
+</html>
+`
